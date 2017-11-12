5deb8549281de4fba1385018e926846408432066
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-11-12 20:06:49 +0900

    initial import

---
+++ lib/ansible/vault.rb
@@ -0,0 +1,41 @@
+require "open3"
+require "yaml"
+
+class PasswordNotSetError < RuntimeError
+end
+
+module Ansible
+  # helper mthods to decrypt files by ansible-vault
+  module Vault
+    @command = "ansible-vault decrypt --output='-'"
+
+    # Decrypts encrypted file using ansible-vault
+    #
+    # @param [String] path to file to decrypt
+    # @return [String] decrypted content of the file
+    def self.decrypt(file:)
+      raise_if_env_is_not_set
+      Open3.popen3("#{@command} '#{file}'") do |_i, o, e, thr|
+        unless thr.value.success?
+          msg = "failed to run `%s`: status: `%s` stderr: `%s`"
+          raise format(msg, @command, thr.value.to_i, e.read)
+        end
+        o.read
+      end
+    end
+
+    # Decrypts encrypted file as YAML using ansible-vault
+    #
+    # @param [String] path to file to decrypt
+    # @return [Hash] decrypted content of the file in a Hash
+    def self.decrypt_as_yaml(file:)
+      YAML.safe_load(decrypt(file: file))
+    end
+
+    # Raises an exception when mandatory environment variable is not set
+    def self.raise_if_env_is_not_set
+      msg = "env variable `ANSIBLE_VAULT_PASSWORD_FILE` is not defined"
+      raise PasswordNotSetError, msg unless ENV["ANSIBLE_VAULT_PASSWORD_FILE"]
+    end
+  end
+end
---
+++ spec/ansible/vault_spec.rb
@@ -0,0 +1,75 @@
+RSpec.describe Ansible::Vault do
+  it "has a version number" do
+    expect(Ansible::Vault::VERSION).not_to be nil
+  end
+
+  def ansible_is_available?
+    `which ansible`.length > 1
+  end
+
+  current_dir = Pathname.new(__FILE__).dirname
+  password_file = current_dir + "key.txt"
+
+  context "with ANSIBLE_VAULT_PASSWORD_FILE set" do
+    before(:all) { ENV["ANSIBLE_VAULT_PASSWORD_FILE"] = password_file.to_s }
+    after(:all) { ENV.delete("ANSIBLE_VAULT_PASSWORD_FILE") }
+
+    describe "#decrypt" do
+      let(:plain_file) { current_dir + "plain.yml" }
+      let(:encrypted_file) { current_dir + "encrypted.yml" }
+      let(:original) { File.read(plain_file) }
+
+      it "decrypts without exceptions" do
+        skip "ansible is not available" unless ansible_is_available?
+        expect { Ansible::Vault.decrypt(file: encrypted_file) }.not_to raise_exception
+      end
+
+      it "decrypts encrypted_file and the content is identical" do
+        skip "ansible is not available" unless ansible_is_available?
+        expect(Ansible::Vault.decrypt(file: encrypted_file)).to eq original
+      end
+    end
+
+    describe "#decrypt_as_yaml" do
+      let(:plain_file) { current_dir + "plain.yml" }
+      let(:encrypted_file) { current_dir + "encrypted.yml" }
+      let(:original) { File.read(plain_file) }
+      let(:original_yaml) { YAML.safe_load(original) }
+
+      it "decrypts without exceptions" do
+        expect { Ansible::Vault.decrypt_as_yaml(file: encrypted_file) }.not_to raise_exception
+      end
+
+      it "decrypts encrypted_file as YAML and the content is identical" do
+        skip "ansible is not available" unless ansible_is_available?
+        expect(Ansible::Vault.decrypt_as_yaml(file: encrypted_file)).to eq original_yaml
+      end
+    end
+  end
+  context "with ANSIBLE_VAULT_PASSWORD_FILE set" do
+    before(:all) { ENV.delete("ANSIBLE_VAULT_PASSWORD_FILE") }
+
+    describe "#decrypt" do
+      let(:plain_file) { current_dir + "plain.yml" }
+      let(:encrypted_file) { current_dir + "encrypted.yml" }
+      let(:original) { File.read(plain_file) }
+
+      it "raises exception" do
+        skip "ansible is not available" unless ansible_is_available?
+        expect { Ansible::Vault.decrypt(file: encrypted_file) }.to raise_exception PasswordNotSetError
+      end
+    end
+
+    describe "#decrypt_as_yaml" do
+      let(:plain_file) { current_dir + "plain.yml" }
+      let(:encrypted_file) { current_dir + "encrypted.yml" }
+      let(:original) { File.read(plain_file) }
+      let(:original_yaml) { YAML.safe_load(original) }
+
+      it "raises exception" do
+        skip "ansible is not available" unless ansible_is_available?
+        expect { Ansible::Vault.decrypt_as_yaml(file: encrypted_file) }.to raise_exception PasswordNotSetError
+      end
+    end
+  end
+end
